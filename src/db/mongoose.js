d1fd1fbf765a6f15092861786e92516e23c30279
Krambeck <leonardokrambeck@gmail.com>
2021-06-09 15:15:48 -0300

    Test db connection

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 
+url = "mongodb+srv://taskapp:<mydbpassword99>@cluster0.wakeh.mongodb.net/task-manager-app?retryWrites=true&w=majority"
 
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
